15414b8d12bd92ee51fdae40caabd681ac483f94
nahid pagheh <nahidpagheh1@gmail.com>
2023-04-07 15:40:48 +0430

    list page

---
+++ components/listFilterPage/cardList.tsx
@@ -0,0 +1,52 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Link from "next/link";
+import styles from "@/components/listFilterPage/list.module.scss";
+interface Props {
+  list;
+  cardLink: string;
+}
+const CardList: React.FC<Props> = ({ list, cardLink }) => {
+  return (
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+    >
+      {list?.map((item) => (
+        <Grid item xs={2} sm={4} md={4} key={item.id}>
+          <Link href={`${cardLink}/${item.id}`}>
+            <Card key={item.id} sx={{ maxWidth: 345 }}>
+              <CardActionArea>
+                {item.image && (
+                  <CardMedia
+                    component="img"
+                    height="auto"
+                    image={item.image}
+                    alt="green iguana"
+                  />
+                )}
+
+                <CardContent>
+                  <Typography
+                    className={styles["card-content"]}
+                    gutterBottom
+                    variant="h5"
+                    component="div"
+                  >
+                    {item.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Link>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+export default CardList;
---
+++ components/listFilterPage/index.tsx
@@ -0,0 +1,108 @@
+import React, { FC, useEffect } from "react";
+import { fetchPullCharacters } from "@/redux/slices/charactersSlice";
+import { fetchPullEpisodes } from "@/redux/slices/episodesSlice";
+import { fetchPullLocations } from "@/redux/slices/locationsSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/store";
+import { ICharacter, ILocation } from "@/redux/types";
+import Sidebar from "@/components/Sidebar/Sidebar";
+import { DataTypes } from "@/types/types";
+import WithHeaderLayout from "@/components/layout/withHeaderLayout/withHeaderLayout";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import "@/components/listFilterPage/list.module.scss";
+import CardList from "@/components/listFilterPage/cardList";
+import Pagination from "@mui/material/Pagination";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { BASE_URL } from "@/urls/baseURLs";
+
+interface Props {
+  type: string;
+}
+
+const List: FC<Props> = ({ type }) => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPull());
+  }, []);
+
+  const listTypeID = Object.values(DataTypes).indexOf(type);
+
+  const sidebarType = [
+    DataTypes.characters,
+    DataTypes.locations,
+    DataTypes.episodes,
+  ][listTypeID];
+
+  const listTitle = ["Characters", "Locations", "Episodes"][listTypeID];
+
+  const fetchPull = [
+    fetchPullCharacters,
+    fetchPullLocations,
+    fetchPullEpisodes,
+  ][listTypeID];
+
+  const state = useAppSelector((state) => {
+    let slice;
+    switch (listTypeID) {
+      case 0: {
+        slice = state.charactersSlice;
+        break;
+      }
+      case 1: {
+        slice = state.locationsSlice;
+        break;
+      }
+      case 2: {
+        slice = state.episodesSlice;
+        break;
+      }
+    }
+    return slice;
+  });
+
+  const pull = state?.results;
+  const info = state?.info;
+
+  const paginationChangeHandler = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    dispatch(fetchPull(`${BASE_URL}character/?page=${value}`));
+  };
+
+  return (
+    <WithHeaderLayout>
+      <Grid container spacing={{ xs: 2, md: 3 }}>
+        <Grid item xs={12} md={3}>
+          <Sidebar type={sidebarType} />
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <main>
+            <Typography variant="h3" gutterBottom>
+              {listTitle}
+            </Typography>
+            {pull?.length > 0 ? (
+              <CardList cardLink={`/${type}`} list={pull} />
+            ) : (
+              <Box display="flex" justifyContent="center">
+                <CircularProgress />
+              </Box>
+            )}
+          </main>
+        </Grid>
+        <Grid display="flex" justifyContent="center" item xs={12}>
+          <Pagination
+            onChange={paginationChangeHandler}
+            count={info?.count}
+            variant="outlined"
+            shape="rounded"
+          />
+        </Grid>
+      </Grid>
+    </WithHeaderLayout>
+  );
+};
+
+export default List;
