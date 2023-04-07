483c8acff624721ebf2dcf56c454f7ca272b05ca
nahid-76 <nahidpagheh1@gmail.com>
2023-04-07 23:55:31 +0430

    delete image

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
 import WithHeaderLayout from "@/components/layout/withHeaderLayout/withHeaderLayout";
-import styles from "@/styles/global.module.scss";
 import { SITE_TITLE } from "@/constants/messages";
 
 const Home: React.FC = () => {
@@ -14,16 +12,7 @@ const Home: React.FC = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <WithHeaderLayout>
-        <div className={styles["image-container"]}>
-          <Image
-            src="/RickMorty.jpg"
-            layout="fill"
-            className={styles["image"]}
-            alt="character image"
-          />
-        </div>
-      </WithHeaderLayout>
+      <WithHeaderLayout />
     </>
   );
 };
