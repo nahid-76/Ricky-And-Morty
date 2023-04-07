870382ce434e5c4e5bd28de1f842af0ee9bb4592
nahid pagheh <nahidpagheh1@gmail.com>
2023-04-07 15:46:36 +0430

    sidebar filters

---
+++ components/Sidebar/filters/types.ts
@@ -0,0 +1,16 @@
+export interface CharacterData {
+	name?: string;
+	status: string;
+    gender: string;
+    species: string;
+    type: string;
+}
+export interface LocationData {
+	name?: string;
+	dimension: string;
+    type: string;
+}
+export interface EpisodeData {
+	name?: string;
+	episode: string;
+}
