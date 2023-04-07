2379eb334f0a11a4883c987dfd174fb96eda3771
nahid pagheh <nahidpagheh1@gmail.com>
2023-04-07 15:38:57 +0430

    home

---
+++ components/home/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+import WithHeaderLayout from "@/components/layout/withHeaderLayout/withHeaderLayout";
+import styles from "@/styles/global.module.scss";
+import { SITE_TITLE } from "@/messages/constantMessages";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>{SITE_TITLE}</title>
+        <meta name="description" content="Rick And Morti app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <WithHeaderLayout>
+        <div className={styles["image-container"]}>
+          <Image
+            src="/RickMorty.jpg"
+            layout="fill"
+            className={styles["image"]}
+            alt="character image"
+          />
+        </div>
+      </WithHeaderLayout>
+    </>
+  );
+};
+export default Home;
